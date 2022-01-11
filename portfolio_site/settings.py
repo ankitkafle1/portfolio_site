aa510e02b99d8dcac3cfcf1851719ac935d92c5e
Ankit69 <51173448+Ankit69@users.noreply.github.com>
2022-01-11 17:34:30 -0500

    jan112022

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quote_generator'
+    'quote_generator',
+    'portfolio_site',
+    'portfolio',
+    'blog'
 ]
 
 MIDDLEWARE = [
