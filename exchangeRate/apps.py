78133be4a908b45af31f20cdd0c336bdc752543d
Ankit69 <51173448+Ankit69@users.noreply.github.com>
2022-01-13 08:57:40 -0500

    thirdCommit

---
+++ exchangeRate/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class ExchangerateConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'exchangeRate'
--- exchangeRate/views.py
+++ exchangeRate/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'quote_generator/home.html')
+    return render(request, 'exchangeRate/index.html')
